30dc64917e0d3ccc2515a4058f26be4854f2d82e
WillisBucket <walsayed@mail.usf.edu>
2021-01-30 13:07:31 -0500

    Compiled UI w/Jumbotron & FileUpload

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import {Button} from 'react-bootstrap/';
 
 import Value from "./Components/input.jsx";
 import NavbarComp from "./Components/navbar-component.jsx";
-<<<<<<< HEAD
 import FooterComp from "./Components/footer.jsx";
 import FileUpload from "./Components/imgUploadComp.jsx";
-=======
-import StickyFooter from "./Components/footer.jsx";
 import Jumbotron from "./Components/jumbotron-component.jsx";
->>>>>>> c0d1073d2a8f0c73e24eadac74a1567e6b6754e9
 
 class App extends Component {
   state = {
@@ -22,19 +18,7 @@ class App extends Component {
     return (
       <div className="App">
       <NavbarComp/>
-<<<<<<< HEAD
-      <Value label={this.label="Name"}/>
-      <Value label={this.label="Date"}/>
-      <Value label={this.label="Amount"}/>
-      <Value label={this.label="Bank"}/>
-      <Value label={this.label="Routing Number"}/>
-      <Value label={this.label="Account Number"}/>
       <FileUpload/>
-      <FooterComp/>
-=======
-        <Button variant="outline-dark" className="px-md-4 space">Upload</Button>
-        <Button variant="outline-dark" className="px-md-4 space">Submit</Button>
-        <Button variant="outline-dark" className="px-md-4 space">Clear</Button>
       <div className="fields">
         <Value label={this.label="Name"}/>
         <Value label={this.label="Date"}/>
@@ -44,8 +28,7 @@ class App extends Component {
         <Value label={this.label="Account Number"}/>
       </div>
       <Jumbotron/>
-      <StickyFooter/>
->>>>>>> c0d1073d2a8f0c73e24eadac74a1567e6b6754e9
+      <FooterComp/>
       </div>
     );
   }
