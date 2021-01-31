f6364a69945c15233041a282624a2447d34f67b1
Wesbert Edouard <wesbert@mail.usf.edu>
2021-01-31 13:09:48 -0500

    Loading JSON Data into React-App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
 import './App.css';
-import React, {Component} from 'react';
-import {Button} from 'react-bootstrap/';
+import React, {Component, useState} from 'react';
+import {Button, Container, Row, Col} from 'react-bootstrap/';
 import FileUpload from "./Components/imgUploadComp.jsx";
 import Value from "./Components/input.jsx";
 import NavbarComp from "./Components/navbar-component.jsx";
 import FooterComp from "./Components/footer.jsx";
+import { Form, TextField, SelectField, SubmitButton } from './Components/FormElements.jsx';
+import {Check} from "./Checks.js";
+import { checkData } from "./data1.js";
 
-class App extends Component {
-  state = {
-    labels: [
-      {label: 'Fill in text'}
-    ]
+function App() {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    role: ""
+  });
+  
+  const onSubmit = (values, { setSubmitting, resetForm, setStatus }) => {
+    console.log(values);
+    setSubmitting(false);
   }
- 
-  render(){
+
     return (
       <div className="App">
-      <NavbarComp/>
-      <h1>Digital Check Deposits Available Now!</h1>
-      <FileUpload/>
-      <div className="fields">
-        <Value label={this.label="Name"}/>
-        <Value label={this.label="Date"}/>
-        <Value label={this.label="Amount"}/>
-        <Value label={this.label="Bank"}/>
-        <Value label={this.label="Routing Number"}/>
-        <Value label={this.label="Account Number"}/>
-      </div>
-      <FooterComp/>
+        <NavbarComp/>
+            <Container>
+              <Row>
+                <Check/>
+              </Row>
+            </Container>
+        <FooterComp/>
       </div>
     
     );
   }
-}
-
-export default App;
+  
+  export default App;
+  
+  /* <h1>Digital Check Deposits Available Now!</h1>
+  <FileUpload/>
+  <div className="fields">
+    <Value label={this.label="Name"}/>
+    <Value label={this.label="Date"}/>
+    <Value label={this.label="Amount"}/>
+    <Value label={this.label="Bank"}/>
+    <Value label={this.label="Routing Number"}/>
+    <Value label={this.label="Account Number"}/>
+  </div> */
---
+++ src/Checks.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./App.css";
+import { checkData } from "./data1";
+
+export const Check = () => {
+  return (
+    <>
+      <HomePageHeader />
+      <div className="stock-container">
+        {checkData.map((data, key) => {
+          return (
+            <div key={key}>
+            <Image key={key} 
+                Name={data.Name} 
+                Date={data.Date} 
+                WordAmount={data.WordAmount} 
+                NumberAmount={data.NumberAmount}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+const HomePageHeader = () => {
+    return (
+      <header className="header">
+        <h2>Check Data</h2>
+      </header>
+    );
+  };
+
+const Image = ({ Name, Date, WordAmount, NumberAmount }) => {
+if (!Name) return <div />;
+return (
+    <table>
+    <tbody>
+        <tr>
+        <td>
+            <h5>{Name}</h5>
+        </td>
+        <td>
+            <h5>{Date}</h5>
+        </td>
+        <td>
+            <h4>{WordAmount}</h4>
+        </td>
+        <td>
+            <p>{NumberAmount}</p>
+        </td>
+        </tr>
+    </tbody>
+    </table>
+);
+};
