2b47837374d79db38ae0325261c4e026d0c3eee2
Wesbert Edouard <wesbert@mail.usf.edu>
2021-01-31 00:40:10 -0500

    Latest updates to python file

--- amz-textract.py
+++ amz-textract.py
@@ -7,9 +7,9 @@ import csv, json
 from num2words import num2words
 import requests
 
-s3 = boto3.client('s3', region_name = "us-east-2")
-client = boto3.client('textract', region_name = "us-east-2")
-bucket = 'mytextracttestbucket'
+s3 = boto3.client('s3', region_name = "us-east-1")
+client = boto3.client('textract', region_name = "us-east-1")
+bucket = 'amz-textract'
 
 def convertToDict(csvFilePath, jsonFilePath):
     	# create a dictionary
@@ -54,14 +54,14 @@ def detectText(data):
     block = data['Blocks']
     result=[]
     result_str = ""
-    ignore_chars = [';', ':', '!', "*", ']', "[" , '"', "{" , "}", "(", ")", "'", ",", "$ "]
+    ignore_chars = [';', ':', '!', "*", ']', "[" , '"', "{" , "}", "(", ")", "'", ",", "$ ", "$"]
     extractedData = []
     for item in block:
         if item["BlockType"] == "LINE":
             result.append(item["Text"]) #result list for index hard-coding (find better way) ((not needed?))
             result_str += item["Text"] + " "  #result string for regex filtering
 
-    print(result_str)
+    # print(result_str, "\n")
     #Filtering result to find the date in mm/dd/yyyy and mm-dd-yyyy format
     date_str = str(re.findall('(\d{1,2}?-\d{1,2}?-\d{1,4})|(\d{1,2}?/\d{1,2}?/\d{1,4})', result_str))
     date_str = date_str.split(" ")
@@ -79,6 +79,10 @@ def detectText(data):
     #Removing specific characters
     for i in ignore_chars:
         amount_str = amount_str.replace(i, "")
+    
+    for j in amount_str:
+        if j == " ":
+            amount_str = amount_str.replace(j, "")
 
     amount_float = float(amount_str)
     amount_float = ("{:.2f}".format(amount_float))
@@ -100,12 +104,12 @@ def auth(file, auth_key, extractedData):
     validated_data = [valid['Date'], valid['Amount in word form'], valid['Amount in number form']]
     # print(f"\nValidation data from csv for file {image_name}: ", "\nDate: " + validated_data[0], "\nAmount in words: " + validated_data[1] , "\nAmount in digits: " + validated_data[2] + "\n")
     # print("Extracted data from image: ", extractedData)
-    x = 0
-    for x in range(len(validated_data)):
-        if extractedData[x] != validated_data[x]:
-            print(f"File {image_name} " + extractedData[x] + " is not validated\n")
-        else:
-            print(f"File {image_name} " + extractedData[x] + " was validated successfully\n")
+    # x = 0
+    # for x in range(len(validated_data)):
+    #     if extractedData[x] != validated_data[x]:
+    #         print(f"File {image_name} " + extractedData[x] + " is not validated\n")
+    #     else:
+    #         print(f"File {image_name} " + extractedData[x] + " was validated successfully\n")
                 
                 
 
@@ -123,20 +127,21 @@ def testAllChecks(csvFile):
 def main():
     # Decide the two file paths according to your 
     # computer system
-    csvFilePath = r'ImageApp\data.csv'
+    csvFilePath = r'data.csv'
     jsonFilePath = r'dataJSON.json'
     csvData = convertToDict(csvFilePath, jsonFilePath)  
     
-    # document = 'check_10.png'
+    # document = ''
     # data = text(client, bucket, document)
     # extractedData = detectText(data)
     # auth(csvData, document, extractedData)
     
     # s3.delete_object(
-    #     Key='check_11.png', 
+    #     Key='check_59.png', 
     #     Bucket='amz-textract')
     
     testAllChecks(csvData)
+    print("All images extracked successfully")
     
     
 
